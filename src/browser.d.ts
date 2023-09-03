60b49b8f59d4ba6da129967a45bc8b718e723c86
pavelsavara <pavel.savara@gmail.com>
2023-09-03 20:19:38 +0200

    export types for browser api

---
+++ src/browser.d.ts
@@ -0,0 +1,4 @@
+import { generate, generateTypes, GenerateOptions, Transpiled, ExportType } from '../obj/js-component-bindgen-component';
+
+export { generate as transpile, generateTypes, GenerateOptions, Transpiled, ExportType };
+
